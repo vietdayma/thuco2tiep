d08ffb0874e11caa5243f1668c13d8c7c59562d2
vietdayma <whitesvux@gmail.com>
2025-04-25 18:23:21 +0700

    lan 13

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,8 @@
+import os
+
 workers = 4
 threads = 2
-bind = "0.0.0.0:10000"
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
 worker_class = "gthread"
 worker_connections = 1000
 timeout = 120
